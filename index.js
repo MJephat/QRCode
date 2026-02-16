4e833a73cb2d588ac7b353f38c17715dea1a031e
jephat <mukundijephat22@gmail.com>
2026-02-16 12:05:26 +0300

    package.json

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ app.get("/pay/1100", (req, res) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Pay KES 1,000</title>
-        <title>Kitty Classic at Muthaiga</title>
         <style>
           body {
             margin: 0;
